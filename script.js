7f0db673dfde9de69ecaa47dad765af502354f9f
pinko <agaslota@wp.pl>
2017-06-26 21:58:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	var url = 'https://restcountries.eu/rest/v1/name/';
+	var countriesList = $('.list1');
+	var countryList2 = $ ('.list2');
+	var landName = $('#landname');
+	var flagPict = $('#flag');
+	
+	$('.title').hide();
+	$('#search').click(searchCountries);
+	$('.countries').hide();
+	function searchCountries() {
+		var countryName = $('#country-name').val();
+	if(!countryName.length) countryName = "Poland";
+	$.ajax({
+		url: url + countryName,
+	  	method: 'GET',
+	  	success: showCountriesList
+		});
+	}	
+	
+	function showCountriesList(resp) {
+	  	countriesList.empty();
+	  	countryList2.empty();
+	  	landName.empty();
+	  	$('.title').show();
+	  	$('.countries').show();
+	  	resp.forEach(function(item) {
+   		//Here is the code that will execute on each successive item in the collection. A single item is hidden under an item variable.
+   		$('<h3>').text(item.name).appendTo(landName);
+   		$('<li>').html('<span>Capital:    </span>' + item.capital).appendTo(countriesList);
+   		$('<li>').html('<span>Currencies:    </span>' + item.currencies).appendTo(countriesList);
+   		$('<li>').html('<span>Region:    </span>' + item.region).appendTo(countriesList);
+		$('<li>').html('<span>Population:    </span>' + item.population).appendTo(countryList2);
+		$('<li>').html('<span>Language:    </span>' + item.languages).appendTo(countryList2);
+		$('<li>').html('<span>Country Area:    </span>' + item.area).appendTo(countryList2);
+		$('<img>').attr('src', item.flag).appendTo(flagPict);
+		
+		});
+	}
+
+});
