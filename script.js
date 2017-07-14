f44955ec04b65ffe587da7541bd5203d66ec8026
pinko <agaslota@wp.pl>
2017-07-14 13:27:42 +0100

    last corrections

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ $(document).ready(function() {
 	var countriesList = $('.list1');
 	var countryList2 = $ ('.list2');
 	var landName = $('#landname');
-	var flagPict = $('#flag');
 	
 	$('.title').hide();
 	$('#search').click(searchCountries);
 	$('.countries').hide();
 	function searchCountries() {
 		var countryName = $('#country-name').val();
-	if(!countryName.length) countryName = "Poland";
-	$.ajax({
-		url: url + countryName,
-	  	method: 'GET',
-	  	success: showCountriesList
+		if(!countryName.length) countryName = "Poland";
+		$.ajax({
+			url: url + countryName,
+		  	method: 'GET',
+		  	success: showCountriesList
 		});
 	}	
 	
@@ -25,17 +24,26 @@ $(document).ready(function() {
 	  	$('.title').show();
 	  	$('.countries').show();
 	  	resp.forEach(function(item) {
-   		//Here is the code that will execute on each successive item in the collection. A single item is hidden under an item variable.
-   		$('<h3>').text(item.name).appendTo(landName);
-   		$('<li>').html('<span>Capital:    </span>' + item.capital).appendTo(countriesList);
-   		$('<li>').html('<span>Currencies:    </span>' + item.currencies).appendTo(countriesList);
-   		$('<li>').html('<span>Region:    </span>' + item.region).appendTo(countriesList);
-		$('<li>').html('<span>Population:    </span>' + item.population).appendTo(countryList2);
-		$('<li>').html('<span>Language:    </span>' + item.languages).appendTo(countryList2);
-		$('<li>').html('<span>Country Area:    </span>' + item.area).appendTo(countryList2);
-		$('<img>').attr('src', item.flag).appendTo(flagPict);
-		
+	   		$('<h3>').text(item.name).appendTo(landName);
+	   		$('<li>').html('<span>Capital:    </span>' + item.capital).appendTo(countriesList);
+	   		$('<li>').html('<span>Currencies:    </span>' + item.currencies).appendTo(countriesList);
+	   		$('<li>').html('<span>Region:    </span>' + item.region).appendTo(countriesList);
+			$('<li>').html('<span>Population:    </span>' + item.population).appendTo(countryList2);
+			$('<li>').html('<span>Language:    </span>' + item.languages).appendTo(countryList2);
+			$('<li>').html('<span>Country Area:    </span>' + item.area).appendTo(countryList2);
 		});
 	}
-
+	$('a').on('click', function(event) {
+		if (this.hash !== "") {
+		    event.preventDefault();
+		    var hash = this.hash;
+		    $('html, body').animate({
+			    scrollTop: $(hash).offset().top}, 
+			    1200, 
+			    function(){
+		        	window.location.hash = hash;
+		    	}
+		    );
+		}
+	});
 });
